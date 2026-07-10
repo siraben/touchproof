c542cbef8724e9fc01d6d4339d997f53c44eadcd
Ben Siraphob <bensiraphob@gmail.com>
2026-07-10 16:19:31 -0700

    chore: establish TouchProof Nix workspace

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "TouchProof development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "aarch64-darwin" "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            packages = with pkgs; [ nodejs_22 git ];
+            shellHook = ''
+              export NEXT_TELEMETRY_DISABLED=1
+              export COREPACK_HOME="$PWD/.cache/corepack"
+              echo "TouchProof: Node $(node --version); use corepack pnpm"
+            '';
+          };
+        });
+    };
+}
